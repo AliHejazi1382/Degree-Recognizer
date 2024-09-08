c0b62df709ed04380b005f9fb725cdddf194e652
AliHejazi1382 <mrhjz08@gmail.com>
2024-09-08 21:39:33 +0330

    project ended

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from utils import *
 import numpy as np
 
-x_train, x_cv, x_test, y_train, y_cv, y_test = get_data()
 
-train_mses, cv_mses =  choose_degree(x_train, x_cv, y_train, y_cv)
-plot_Jcv_Jtrain(train_mses, cv_mses)
-print(cv_mses)
-print(train_mses)
+x_train, x_cv, x_test, y_train, y_cv, y_test = get_data()
+best_alpha, best_degree = choose_best_hyperparameters(x_train, x_cv, y_train, y_cv)
+print(f'{best_alpha} {best_degree}')
+
+plot_data(x_train,y_train, 12, best_alpha)
+
+
--- utils.py
+++ utils.py
@@ -4,8 +4,10 @@ import os
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
 
 import matplotlib.pyplot as plt
 dlc = dict(dlblue = '#0096ff', dlorange = '#FF9300', dldarkred='#C00000', dlmagenta='#FF40FF', dlpurple='#7030A0', dldarkblue =  '#0D5BDC')
@@ -49,18 +51,53 @@ def choose_degree(x_train, x_cv, y_train, y_cv):
 
     return train_mses, cv_mses
 
-def plot_Jcv_Jtrain(train_mses, cv_mses):
-    degrees = range(1, 11)
-    optimal_degree = np.argmin(cv_mses) + 1
-    plt.plot(degrees, train_mses, c='r', marker='o', label='Training MSEs')
-    plt.plot(degrees, cv_mses, c='b', marker='o', label='Cross validation MSEs')
-    plt.axvline(optimal_degree, lw=1, color = dlc["dlmagenta"])
-    plt.annotate("optimal degree", xy=(optimal_degree,200),xycoords='data',
-                xytext=(0.5, 0.4), textcoords='axes fraction', fontsize=10,
-                   arrowprops=dict(arrowstyle="->", connectionstyle="arc3", 
-                                   color=dlc['dldarkred'], lw=1))
-    plt.title('degree of polynomial vs. train and CV MSEs')
-    plt.xlabel('degrees')
-    plt.ylabel('MSEs')
+def choose_best_hyperparameters(x_train, x_cv, y_train, y_cv):
+    pipeline = Pipeline([
+        ('poly', PolynomialFeatures(include_bias=False)),
+        ('scaler', StandardScaler()),
+        ('model', Ridge())
+    ])
+    
+    # Updated parameter grid with larger alpha values to avoid small values that cause instability
+    param_grid = {
+        'poly__degree': range(1, 11),
+        'model__alpha': [1e-3, 1e-2, 0.1, 1, 10, 100]
+    }
+    
+    grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring='neg_mean_squared_error')
+    
+    grid_search.fit(x_train, y_train)
+    
+    best_degree = grid_search.best_params_['poly__degree']
+    best_lambda = grid_search.best_params_['model__alpha']
+    
+    return best_lambda, best_degree
+
+def predict(x_train, y_train, degree, alpha):
+    poly = PolynomialFeatures(degree=degree, include_bias=False)
+    x_train_mapped = poly.fit_transform(x_train)
+
+    scaler = StandardScaler()
+    x_train_mapped_scaled = scaler.fit_transform(x_train_mapped)
+
+    model = Ridge(alpha=alpha)
+    model.fit(x_train_mapped_scaled, y_train)
+    return model.predict(x_train_mapped_scaled)
+
+def plot_data(x_train, y_train, best_degree, best_alpha):
+    # Sort the data
+    sorted_indices = np.argsort(x_train.flatten())
+    x_train_sorted = x_train[sorted_indices]
+    y_train_sorted = y_train[sorted_indices]
+
+    yhat = predict(x_train_sorted, y_train_sorted, best_degree, best_alpha)
+
+    plt.figure(figsize=(8, 6))
+    plt.scatter(x_train_sorted, y_train_sorted, color='blue', label='Training Data')
+    plt.plot(x_train_sorted, yhat, color='red', label=f'Best Fit: degree={best_degree}, alpha={best_alpha}')
+    plt.title('Polynomial Regression Fit')
+    plt.xlabel('X')
+    plt.ylabel('Y')
     plt.legend()
+    
     plt.show()
